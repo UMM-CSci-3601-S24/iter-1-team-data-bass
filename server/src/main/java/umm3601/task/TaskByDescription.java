9abcd06e3f28968ef4059483e79da998bc0b154b
Ethan Graybar <grayb031@morris.umn.edu>
2024-02-24 12:48:37 -0600

    TaskByDescription info filled in

---
+++ server/src/main/java/umm3601/task/TaskByDescription.java
@@ -0,0 +1,8 @@
+package umm3601.task;
+
+@SuppressWarnings("checkstyle:visibilitymodifier")
+public class TaskByDescription {
+  @SuppressWarnings("checkstyle:taskdescription")
+  public String _id;
+  public String name;
+}
