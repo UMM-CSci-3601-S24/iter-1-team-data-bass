faae613796e99da324de211f31541a75ae33bbe0
Ethan Graybar <grayb031@morris.umn.edu>
2024-02-24 12:39:13 -0600

    Task.java filled in

---
+++ server/src/main/java/umm3601/task/Task.java
@@ -0,0 +1,33 @@
+package umm3601.task;
+
+import org.mongojack.Id;
+import org.mongojack.ObjectId;
+
+@SuppressWarnings({"VisibilityModifier"})
+public class Task {
+
+  @ObjectId @Id
+
+  @SuppressWarnings({"TaskDescription"})
+  public String _id;
+  public String description;
+  public int position;
+  public String Huntid;
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof Task)) {
+      return false;
+    }
+    Task other = (Task) obj;
+    return _id.equals(other._id);
+  }
+
+  @Override
+  public int hashCode() {
+    // Equal tasks will hash the same.
+    return _id.hashCode();
+  }
+
+
+}
