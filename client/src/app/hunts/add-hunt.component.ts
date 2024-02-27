ec7d4d0cfc3609afd8de924a0bca7ecd513e1f72
Simon <simonhm31@gmail.com>
2024-02-27 15:47:22 -0600

    generated everything in hunts folder

---
+++ client/src/app/hunts/add-hunt.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-hunt',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-hunt.component.html',
+  styleUrl: './add-hunt.component.scss'
+})
+export class AddHuntComponent {
+
+}
---
+++ client/src/app/hunts/hunt-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hunt-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './hunt-profile.component.html',
+  styleUrl: './hunt-profile.component.scss'
+})
+export class HuntProfileComponent {
+
+}
