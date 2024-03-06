101482d2411b21efb593009eabdd9f685db42afa
Jack Mahoney <mahon480@morris.umn.edu>
2024-03-06 14:59:16 -0600

    fixed tests

--- client/src/app/player/player.component.spec.ts
+++ client/src/app/player/player.component.spec.ts
@@ -11,7 +11,7 @@ describe('PlayerComponent', () => {
       imports: [PlayerComponent]
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(PlayerComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +19,23 @@ describe('PlayerComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should add a new textarea when addTextarea is called', () => {
+    const initialLength = component.textAreasList.length;
+    component.addTextarea();
+    expect(component.textAreasList.length).toBe(initialLength + 1);
+    expect(component.textAreasList[initialLength]).toBe('text_area' + (initialLength + 1));
+  });
+
+  it('should remove a textarea when removeTextArea is called', () => {
+    // Add a textarea to the list
+    component.addTextarea();
+    const initialLength = component.textAreasList.length;
+
+    // Remove the textarea we just added
+    component.removeTextArea(initialLength - 1);
+
+    expect(component.textAreasList.length).toBe(initialLength - 1);
   });
 });
